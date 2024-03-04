3a9694a7d9b4d174991a4ced3319d5bd84cf378f
Micha <michael.rigas@t-online.de>
2024-03-04 15:39:17 +0100

    console.log laufen lassen

--- unit-testing/calculator.js
+++ unit-testing/calculator.js
@@ -7,6 +7,8 @@ function divide(a, b) {
   return a / b;
 }
 
+console.log(sum(5, 3)); // Ausgabe: 8
+console.log(divide(10, 2)); // Ausgabe: 5
 // Im Browser/in Javascript: export default ...
 // Wir sind hier aber in NodeJS!
 module.exports.sum = sum;
